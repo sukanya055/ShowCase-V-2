87b603810d8a918e375b647a974336a8de337fb7
Rahul Pradhan <pradhanrahul3346@gmail.com>
2022-10-08 16:27:13 +0530

    login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Layout } from "../components";
 import { FcGoogle } from "react-icons/fc";
 import { motion } from "framer-motion";
@@ -7,7 +7,10 @@ import img2 from "../assets/img2.png";
 import img6 from "../assets/img6.png";
 import img4 from "../assets/img4.png";
 import img5 from "../assets/img5.png";
-
+const initialState = {
+  email: "",
+  password: "",
+};
 const Login = () => {
   const loaderVariants = {
     animationOne: {
@@ -45,44 +48,69 @@ const Login = () => {
       },
     },
   };
+  const [formData, setFormData] = useState(initialState);
+  const [isLogin, setIsLogin] = useState(true);
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    setFormData(initialState);
+  };
+
   return (
     <Layout>
       <div className="bg-gradient-to-tl h-full flex items-center justify-center from-sky-200 to-sky-100 px-14 py-8">
         <div className="flex flex-row  h-auto w-2/3 shadow-white shadow ">
           <div className="basis-1/2 bg-gradient-to-tl px-14 py-5 from-[#5151E5] text-white to-[#72EDF2]">
             <h1 className="text-center text-xl font-bold my-2 font-sans mt-8">
-              Login
+              {isLogin ? "Login" : "Create Account"}
             </h1>
             <h3 className="text-center font-roboto text-md">
               Please Login using account details bellow
             </h3>
-            <form className="  mt-12 flex-col">
+            <form onSubmit={handleOnSubmit} className="mt-12 flex-col">
               <input
                 type="email"
                 placeholder="Enter Email"
                 required
+                value={formData.email}
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
                 className=" border-b-[1px] placeholder-white outline-none text-white bg-transparent w-full input-md border-white max-w-xs"
               />
               <input
                 type="password"
                 placeholder="Enter Password"
                 required
+                value={formData.password}
+                onChange={(e) =>
+                  setFormData({ ...formData, password: e.target.value })
+                }
                 className=" border-b-[1px] placeholder-white outline-none text-white bg-transparent w-full input-md border-white max-w-xs"
               />
               <p className="text-white cursor-pointer text-sm mt-5">
                 Forgot your password ?
               </p>
-              <button className="font-meduim rounded-full mt-5 py-2 justify-self-end  w-[160px] bg-white text-black">
-                SignIn
-              </button>
+              <div className="w-full flex justify-end items-center">
+                <button className="font-meduim rounded-full mt-5 py-2 justify-self-end  w-[160px] bg-white text-black">
+                  {isLogin ? "SignIn" : "Signup"}
+                </button>
+              </div>
             </form>
             <div className="divider devide-bg-white divide-white mt-5">OR</div>
             <button className="btn w-full bg-white mt-4 text-black capitalize">
               <FcGoogle className="text-xl mr-2 " /> Register with Google
             </button>
-            <p className="text-white cursor-pointer text-md my-5 text-center ">
-              Don't have an account?
-              <span className="font-bold"> Create account</span>
+            <p
+              className="text-white cursor-pointer text-md my-5 text-center "
+              onClick={() => setIsLogin((prev) => !prev)}
+            >
+              {isLogin
+                ? " Don't have an account?"
+                : " already have an account?"}
+              <span className="font-bold">
+                {isLogin ? " Create Account" : " Login"}
+              </span>
             </p>
           </div>
           <div className="basis-1/2   relative bg-white">
