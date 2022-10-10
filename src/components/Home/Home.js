23a5b9a68c23e6642e4786761e22a0f3c4f2c34e
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-10-10 11:05:15 +0600

    lazy load

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,42 @@
-import React from 'react';
-import Amazing from '../Pages/Amazing/Amazing';
+import React, { lazy, Suspense, useEffect } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+/* import Amazing from '../Pages/Amazing/Amazing';
 import Count from '../Pages/Count/Count';
+import WorksAndQuotes from '../Pages/WorksAndQuotes/WorksAndQuotes'; */
+
 import Hero from '../Pages/Hero/Hero';
-import WorksAndQuotes from '../Pages/WorksAndQuotes/WorksAndQuotes';
+import ErrorFallback from '../share/ErrorFallBack';
+const Amazing = lazy(() => import('../Pages/Amazing/Amazing'))
+// const Hero = lazy(() => import('../Pages/Hero/Hero'))
+const Count = lazy(() => import('../Pages/Count/Count'))
+const WorksAndQuotes = lazy(() => import('../Pages/WorksAndQuotes/WorksAndQuotes'))
+
+
 
 const Home = () => {
+
+   
     return (
         <div>
             <Hero />
-            <Amazing /> 
-            <Count/>
-            <WorksAndQuotes/>
+            {/* <PFd/> */}
+            <ErrorBoundary
+                FallbackComponent={ErrorFallback}
+                onReset={() => {
+                    // reset the state of your app so the error doesn't happen again
+                }}
+            >
+
+                <Suspense fallback={<div>
+                    <p className='text-center py-5'>Loading...</p>
+                </div>}>
+                    <section>
+                        <Amazing />
+                        <Count />
+                        <WorksAndQuotes />
+                    </section>
+                </Suspense>
+            </ErrorBoundary>
         </div>
     );
 };
