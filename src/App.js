f23d1c29d6968034644b29a02dcbdf6a4d49c66c
Rahul Pradhan <pradhanrahul3346@gmail.com>
2022-10-10 14:57:42 +0530

    remove home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import { Home, Product, Login } from "./pages";
+import { Product, Login } from "./pages";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
           <Route path="/product/:id" element={<Product />} />
           <Route path="/login" element={<Login />} />
         </Routes>
