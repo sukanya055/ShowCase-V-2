3f6a39ec1b86b7c2fe4faef4696056ae0f602ba8
Rahul Pradhan <pradhanrahul3346@gmail.com>
2022-10-18 15:42:04 +0530

    fix issues

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
   CompleteProfile,
   OtpVerify,
   JoinUs,
+  Products,
   // ChangePassword
 } from "./pages";
 
@@ -21,25 +22,30 @@ import BusinessChangePassword from "./pages/BusinessChangePassword";
 import Gps from "./pages/Gps";
 import ChatAdmin from "./components/ChatFile/ChatAdmin";
 
-
 function App() {
   return (
     <div className="App">
       <Routes>
         {/* support chat route For admin */}
-        <Route path='/supportChat' element={<ChatAdmin />} />
+        <Route path="/supportChat" element={<ChatAdmin />} />
         <Route path="/product/:id" element={<Product />} />
         <Route path="/auth" element={<Login />} />
 
         <Route path="/" element={<Home />} />
 
-        <Route path="dashboard"  >
-          <Route path='updateProfile' element={<NormalProfile />} />
-          <Route path='updatePasswordNormalProfile' element={<ChangePassword />} />
+        <Route path="dashboard">
+          <Route path="updateProfile" element={<NormalProfile />} />
+          <Route
+            path="updatePasswordNormalProfile"
+            element={<ChangePassword />}
+          />
         </Route>
 
-        <Route path="/businessProfile"   >
-          <Route path="updateBusinessPassword" element={<BusinessChangePassword />} />
+        <Route path="/businessProfile">
+          <Route
+            path="updateBusinessPassword"
+            element={<BusinessChangePassword />}
+          />
           <Route path="updateBusinessProfile" element={<BusinessProfile />} />
           <Route path="updateGps" element={<Gps />} />
         </Route>
@@ -48,18 +54,16 @@ function App() {
 
         <Route path="/changePassword" element={<PasswordChange />} />
 
-        <Route path="/businessAccountSetup" element={<BussinessAccountSetup />} />
+        <Route
+          path="/businessAccountSetup"
+          element={<BussinessAccountSetup />}
+        />
         <Route path="/normalAccountSetup" element={<BussinessAccountSetup />} />
 
         <Route path="/completeProfile" element={<CompleteProfile />} />
         <Route path="/otpVerify" element={<OtpVerify />} />
         <Route path="/joinUs" element={<JoinUs />} />
-
-
-
-
-
-
+        <Route path="/products" element={<Products />} />
       </Routes>
     </div>
   );
