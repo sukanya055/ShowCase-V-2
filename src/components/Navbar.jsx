fd302b557cbdfdb32dda1d6c2d0003318061f570
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-10-22 11:09:57 +0600

    my account link complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../assets/logo.png";
 import { BiSearchAlt, BiUser } from "react-icons/bi";
 import { FiChevronDown } from "react-icons/fi";
@@ -16,13 +16,59 @@ import {
   mensCategories,
   womenCategories,
 } from "../utils/data";
+import { useCookies } from "react-cookie";
+import axios from "axios";
 // >>>>>>> 606301900deb403c949899cf94cbf7d5f0d3b2c2
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [search, setSearch] = useState("");
   const navigate = useNavigate();
+  const [cookies, setCookie, removeCookie] = useCookies(["token"]);
   const toggle = () => {
     setIsOpen((prev) => !prev);
+  };
+
+  useEffect(() => {
+    (async () => {
+      if (cookies?.token) {
+        try {
+          const { data } = await axios.get("http://localhost:5000/user/infor", {
+            headers: {
+              Authorization: cookies?.token,
+            },
+          });
+          console.log(data);
+        } catch (err) {
+          console.log(err);
+        }
+      } else {
+        alert("Login please");
+      }
+    })();
+  }, [cookies]);
+
+  const handleDashboard = async () => {
+    if (cookies?.token) {
+      try {
+        const { data } = await axios.get("http://localhost:5000/user/infor", {
+          headers: {
+            Authorization: cookies?.token,
+          },
+        });
+        console.log(data);
+        if(data?.role === 1){
+          navigate('/businessProfile/businessDashboard')
+        }
+        if(data?.role === 0){
+          navigate('/dashboard/normalDashboard')
+        }
+
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      alert("Login please");
+    }
   };
 
   const token = localStorage.getItem("token");
@@ -31,6 +77,7 @@ const Navbar = () => {
   const handleSignOut = () => {
     signOut(auth);
     localStorage.removeItem("token");
+    removeCookie("token");
     navigate("/");
   };
 
@@ -40,7 +87,7 @@ const Navbar = () => {
       <div className="flex items-center justify-between">
         {/* Logo */}
         <div className="flex gap-1 md:gap-8 items-center justify-between ">
-          <img src={logo} alt="logo" className="h-[57px] w-[110px]" />
+          <img onClick={()=>navigate('/')} src={logo} alt="logo" className="h-[57px] w-[110px] cursor-pointer" />
           {/* Search  */}
           <div className="md:w-auto w-[130px]  bg-gray-100 py-2  rounded-2xl">
             <label className="input-group">
@@ -162,7 +209,7 @@ const Navbar = () => {
                 tabIndex={0}
                 className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-auto"
               >
-                <li className="w-32">
+                <li onClick={handleDashboard} className="w-32">
                   <Link to="/dashboard/normalDashboard">My Account</Link>
                 </li>
               </ul>
--- src/pages/modal/PostVideo.js
+++ src/pages/modal/PostVideo.js
@@ -3,12 +3,17 @@ import React, { useState } from 'react';
 const PostVideo = ({ openModal, setOpenModal }) => {
 
     const [video, setVideo] = useState('')
-
+    const [formData, setFormData] = useState({})
 
     const videoHandler = (event) => {
         if (event.target.files && event.target.files[0]) {
             setVideo(URL.createObjectURL(event.target.files[0]));
         }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(formData)
     }
 
 
@@ -20,78 +25,104 @@ const PostVideo = ({ openModal, setOpenModal }) => {
                 <div className="modal-box w-[800px]">
                     <h5 className='text-center my-10 text-2xl font-bold'>Post Your Video</h5>
                     <div className="card-body">
-                        <div className='text-center mb-8 '>
-                            <input onChange={videoHandler} className='hidden' type="file" name="" id="video" required />
-                            <label className='btn bg-[#858A89] rounded-full px-9 py-2 capitalize text-white' htmlFor="video">Choose Video</label>
-                        </div >
-                        {
-                            video && <div className='flex justify-center mb-7'>
-                                <video
-                                    className=''
-                                    src={video}
-                                    autoPlay={true}
-                                    muted={true}
-                                    width='220px'
-                                    height={'140px'}
-                                    loop={true}
-                                ></video>
+                        <form onSubmit={handleSubmit}>
+                            <div className='text-center mb-8 '>
+                                <input onChange={videoHandler} className='hidden' type="file" id="video" />
+                                <label className='btn bg-[#858A89] rounded-full px-9 py-2 capitalize text-white' htmlFor="video">Choose Video</label>
+                            </div >
+                            {
+                                video && <div className='flex justify-center mb-7'>
+                                    <video
+                                        className=''
+                                        src={video}
+                                        autoPlay={true}
+                                        muted={true}
+                                        width='220px'
+                                        height={'140px'}
+                                        loop={true}
+                                    ></video>
+                                </div>
+                            }
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Company Name</span>
+                                </label>
+                                <input
+                                    value={formData.companyName}
+                                    onChange={e => setFormData({ ...formData, companyName: e.target.value })}
+                                    type="text"
+                                    className="input input-bordered"
+                                    required
+                                />
                             </div>
-                        }
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Company Name</span>
-                            </label>
-                            <input type="text" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input type="text" className="input input-bordered" required />
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input
+                                    value={formData.email}
+                                    onChange={e => setFormData({ ...formData, email: e.target.value })}
+                                    type="email" className="input input-bordered" required />
 
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Product Brand(with Model)</span>
-                            </label>
-                            <input type="text" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Product Type</span>
-                            </label>
-                            <input type="text" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Product Brand(with Model)</span>
+                                </label>
+                                <input
+                                    value={formData.productBrand}
+                                    onChange={e => setFormData({ ...formData, productBrand: e.target.value })}
+                                    type="text" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Product Type</span>
+                                </label>
+                                <input
+                                    value={formData.productType}
+                                    onChange={e => setFormData({ ...formData, productType: e.target.value })}
+                                    type="text" className="input input-bordered" required />
 
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Category</span>
-                            </label>
-                            <select className="select select-bordered w-full " >
-                                <option value="shirt">shirt</option>
-                                <option value="shoes">shoes</option>
-                                <option value="pants">pants</option>
-                            </select>
-                        </div>
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Category</span>
+                                </label>
+                                <select
+                                    value={formData.category}
+                                    onChange={e => setFormData({ ...formData, category: e.target.value })}
+                                    className="select select-bordered w-full " >
+                                    <option value="shirt">shirt</option>
+                                    <option value="shoes">shoes</option>
+                                    <option value="pants">pants</option>
+                                </select>
+                            </div>
 
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Product price</span>
-                            </label>
-                            <input type="number" className="input input-bordered" required />
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Product price</span>
+                                </label>
+                                <input
+                                    value={formData.price}
+                                    onChange={e => setFormData({ ...formData, price: e.target.value })}
+                                    type="number" className="input input-bordered" required />
 
-                        </div>
+                            </div>
 
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Product Description</span>
-                            </label>
-                            <textarea className="textarea textarea-bordered" required></textarea>
-                        </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Product Description</span>
+                                </label>
+                                <textarea
+                                    value={formData.description}
+                                    onChange={e => setFormData({ ...formData, description: e.target.value })}
+                                    className="textarea textarea-bordered" required></textarea>
+                            </div>
 
-                        <div className="form-control mt-6">
-                            <button className="btn btn-primary">Post</button>
-                        </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Post</button>
+                            </div>
+                        </form>
                     </div>
 
                     <div className="modal-action ">
